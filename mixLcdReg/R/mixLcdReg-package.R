9873521a3c9bd5fcab29e20de9e6e69a1d7b2574
sang82496 <sang82496@naver.com>
2024-01-24 22:21:27 -0800

    Add files via upload

---
+++ mixLcdReg/R/mixLcdReg-package.R
@@ -0,0 +1,11 @@
+#' Mixture of log-concave regressions modeling
+#'
+#' This package uses mixture of log-concave regressions to analyze complex and heterogenous data.
+#' 
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
